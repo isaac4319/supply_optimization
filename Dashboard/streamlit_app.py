3b6585b2c18f170bb7bd1ac067d0564ffb4a7719
isaac4319 <110285317+isaac4319@users.noreply.github.com>
2025-07-22 15:08:01 +0000

    Fix: dataset updated

--- Dashboard/streamlit_app.py
+++ Dashboard/streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 st.title("Supply Chain Forecast")
 
-df = pd.read_csv("../Data/data/demand.csv")
+df = pd.read_csv("../data/forecasted_demand.csv")
 sku = st.selectbox("Choose SKU", df['sku'].unique())
 wh = st.selectbox("Choose Warehouse", df['warehouse'].unique())
 
 filtered = df[(df['sku'] == sku) & (df['warehouse'] == wh)]
 filtered['date'] = pd.to_datetime(filtered['date'])
-st.line_chart(filtered.set_index('date')['demand_units'])
+st.line_chart(filtered.set_index('date')['forecasted_demand_units'])
